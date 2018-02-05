0e3902f5adf8d1bc5913b894b8df349400645e1e
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-05 19:57:29 +0530

    Commit

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-var app=require('./app.js');
+var inc=require('./app.js');
 
-app.logIncident('Bikash Panigrahi','',function(err,res)
+inc.logIncident('Bikash Panigrahi','',function(err,res)
 {
 console.log(res["result"]);
 });
 
-app.statusIncident('INC0010873',function(err,res){
+inc.statusIncident('INC0010873',function(err,res){
     
     console.log(res);
     
 });
+
+
+var request = require('http');
+var express=require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var portC = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+   app.post('/',function(req,res){
+      console.log('Received the request & it is:::'+JSON.stringify(req.body));
+      if(req.body.result.action=='BookFlight'){
+    
+      var resagent='Your Ticket has been booked ';
+     console.log('request are'+resagent);
+      return res.json({
+        speech:resagent,
+        displayText: resagent,
+        source:'Flight Booking'
+      });
+
+
+    }
+
+});
+
+
+
+app.listen(portC, function(){
+    console.log('AGENT is running my app on  PORT: ' + portC);
+}); 
+
