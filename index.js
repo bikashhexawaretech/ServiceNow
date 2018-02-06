1b424d985563a09c75238ff19ab95e2e5fdd843a
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-06 19:47:38 +0530

    jj

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.post('/',function(req,res){
     console.log('Received the request & it is:::'+JSON.stringify(req.body));
 
- if(req.body.result.action==='Incident_Request.Incident_Request-custom'){
+ if(req.body.result.action==='IncidentRequestAction'){
 
      var facebookResponse={
         "speech": "",
