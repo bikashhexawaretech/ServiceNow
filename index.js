cb6b990200da54179044577625cfa52f1cc34428
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-06 21:11:46 +0530

    hgd

--- index.js
+++ index.js
@@ -118,11 +118,12 @@ if( req.body.result.action=== "Incident_Request.Incident_Request-custom" && req.
 
 
   if( req.body.result.action=== "WebCallAction"){
-  var incID= inc.logIncident("Test Description","",function(err,res){
-   
+  var incID='';
+   inc.logIncident("Test Description","",function(err,res){
+   incID=res["result"];
     
   })
-  var resagent=incID.result.number+" Incident logged Successfully";
+  var resagent=incID+" Incident logged Successfully";
     console.log('request are'+resagent);
      return res.json({
        speech:resagent,
