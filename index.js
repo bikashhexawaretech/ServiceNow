ce849c3f18010bf7a1e07590351afddb3da490c3
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-06 19:10:07 +0530

    hhh

--- index.js
+++ index.js
@@ -16,75 +16,25 @@ app.post('/',function(req,res){
  //if(req.body.result.action==='Incident_Request.Incident_Request-custom'){
      var facebookResponse={
         "speech": "",
-        "displayText": " Your Incident has been raised",
-        "data": {
-          "facebook":{
-        
-            "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "list",
-                "top_element_style": "compact",
-                "elements": [
-                  {
-                    "title": "Classic T-Shirt Collection",
-                    "subtitle": "See all our colors",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                    "buttons": [
-                      {
-                        "title": "View",
-                        "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                        "messenger_extensions": true,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                      }
-                    ]
-                  },
-                  {
-                    "title": "Classic White T-Shirt",
-                    "subtitle": "See all our colors",
-                    "default_action": {
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                      "messenger_extensions": false,
-                      "webview_height_ratio": "tall"
-                    }
-                  },
-                  {
-                    "title": "Classic Blue T-Shirt",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                    "subtitle": "100% Cotton, 200% Comfortable",
-                    "default_action": {
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                    },
-                    "buttons": [
-                      {
-                        "title": "Shop Now",
-                        "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                        "messenger_extensions": true,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                      }
-                    ]        
-                  }
-                ],
-                 "buttons": [
-                  {
-                    "title": "View More",
-                    "type": "postback",
-                    "payload": "payload"            
-                  }
-                ]  
-              }
-            }
+        "messages": [
+          {
+            "type": 2,
+            "platform": "facebook",
+            "title": "Select Sub Categories",
+            "replies": [
+              "1",
+              "2",
+              "3",
+              "4",
+              "5",
+              "7"
+            ]
+          },
+          {
+            "type": 0,
+            "speech": ""
           }
-        }
+        ]
      }
      return res.json(facebookResponse);
 //}
