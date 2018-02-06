0ccc8d4bd8491078fd341693e70ab7ac57b40afd
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-06 21:48:21 +0530

    kk

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var inc = require('./app.js');
 let inn=''
-var abc=inc.logIncident("Test Description","",function(err,res){
+inc.logIncident("Test Description","",function(err,res){
     
-    
+    inn= res.result.number;
    });
-   console.log(abc);
+   console.log(inn);
