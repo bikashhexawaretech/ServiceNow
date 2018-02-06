5390006faba332c5417b3b5fcb42d17203d22ff0
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-06 18:34:39 +0530

    commit

--- index.js
+++ index.js
@@ -10,7 +10,80 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
+app.post('/',function(req,res){
+    console.log('Received the request & it is:::'+JSON.stringify(req.body));
 
+ if(req.body.result.action==='Incident_Request.Incident_Request-custom'){
+     var facebookResponse={
+        "message": {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "list",
+                "top_element_style": "compact",
+                "elements": [
+                  {
+                    "title": "Classic T-Shirt Collection",
+                    "subtitle": "See all our colors",
+                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
+                    "buttons": [
+                      {
+                        "title": "View",
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                      }
+                    ]
+                  },
+                  {
+                    "title": "Classic White T-Shirt",
+                    "subtitle": "See all our colors",
+                    "default_action": {
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                      "messenger_extensions": false,
+                      "webview_height_ratio": "tall"
+                    }
+                  },
+                  {
+                    "title": "Classic Blue T-Shirt",
+                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                    "subtitle": "100% Cotton, 200% Comfortable",
+                    "default_action": {
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    },
+                    "buttons": [
+                      {
+                        "title": "Shop Now",
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                      }
+                    ]        
+                  }
+                ],
+                 "buttons": [
+                  {
+                    "title": "View More",
+                    "type": "postback",
+                    "payload": "payload"            
+                  }
+                ]  
+              }
+            }
+          }
+        }
+     }
+ 
+});
 
 /*app.post('/',function(req,res){
     console.log('Received the request & it is:::'+JSON.stringify(req.body));
@@ -54,10 +127,12 @@ app.use(bodyParser.json());
 return res.json(facebookResponse);
  
     } 
-});*/
+});
+
+*/
 
     //Not working
-    app.post('/',function(req,res){
+ /*   app.post('/',function(req,res){
         console.log('Received the request & it is:::'+JSON.stringify(req.body));
 
    
@@ -90,6 +165,8 @@ return res.json(resObj);
         
 
     });
+
+    */
 
 
 //Not working
