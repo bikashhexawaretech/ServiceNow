b4024d2ef9b747d28f96ad72cfa457c055ab7f0b
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-08 14:24:26 +0530

    jhj

--- index.js
+++ index.js
@@ -190,62 +190,74 @@ if( req.body.result.action=== "Incident_Status_Check"){
    
     facebookResponse={
       "speech": "",
-      "message":{
-        "attachment":{
-          "type":"template",
-          "payload":{
-            "template_type":"receipt",
-            "recipient_name":"Stephane Crozatier",
-            "order_number":"12345678902",
-            "currency":"USD",
-            "payment_method":"Visa 2345",        
-            "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
-            "timestamp":"1428444852",         
-            "address":{
-              "street_1":"1 Hacker Way",
-              "street_2":"",
-              "city":"Menlo Park",
-              "postal_code":"94025",
-              "state":"CA",
-              "country":"US"
-            },
-            "summary":{
-              "subtotal":75.00,
-              "shipping_cost":4.95,
-              "total_tax":6.19,
-              "total_cost":56.14
-            },
-            "adjustments":[
-              {
-                "name":"New Customer Discount",
-                "amount":20
-              },
-              {
-                "name":"$10 Off Coupon",
-                "amount":10
+      "messages": [
+        {
+          "type": 4,
+          "platform": "facebook",
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "receipt",
+                  "recipient_name": "Stephane Crozatier",
+                  "order_number": "12345678902",
+                  "currency": "USD",
+                  "payment_method": "Visa 2345",
+                  "order_url": "http://petersapparel.parseapp.com/order?order_id=123456",
+                  "timestamp": "1428444852",
+                  "address": {
+                    "street_1": "1 Hacker Way",
+                    "street_2": "",
+                    "city": "Menlo Park",
+                    "postal_code": "94025",
+                    "state": "CA",
+                    "country": "US"
+                  },
+                  "summary": {
+                    "subtotal": 75,
+                    "shipping_cost": 4.95,
+                    "total_tax": 6.19,
+                    "total_cost": 56.14
+                  },
+                  "adjustments": [
+                    {
+                      "name": "New Customer Discount",
+                      "amount": 20
+                    },
+                    {
+                      "name": "$10 Off Coupon",
+                      "amount": 10
+                    }
+                  ],
+                  "elements": [
+                    {
+                      "title": "Classic White T-Shirt",
+                      "subtitle": "100% Soft and Luxurious Cotton",
+                      "quantity": 2,
+                      "price": 50,
+                      "currency": "USD",
+                      "image_url": "http://petersapparel.parseapp.com/img/whiteshirt.png"
+                    },
+                    {
+                      "title": "Classic Gray T-Shirt",
+                      "subtitle": "100% Soft and Luxurious Cotton",
+                      "quantity": 1,
+                      "price": 25,
+                      "currency": "USD",
+                      "image_url": "http://petersapparel.parseapp.com/img/grayshirt.png"
+                    }
+                  ]
+                }
               }
-            ],
-            "elements":[
-              {
-                "title":"Classic White T-Shirt",
-                "subtitle":"100% Soft and Luxurious Cotton",
-                "quantity":2,
-                "price":50,
-                "currency":"USD",
-                "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
-              },
-              {
-                "title":"Classic Gray T-Shirt",
-                "subtitle":"100% Soft and Luxurious Cotton",
-                "quantity":1,
-                "price":25,
-                "currency":"USD",
-                "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
-              }
-            ]
+            }
           }
+        },
+        {
+          "type": 0,
+          "speech": "Hi!"
         }
-      }
+      ]
     
    }
    return res.json(facebookResponse);
