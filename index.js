c5636f6d7da790afeeafe7f7dec8e85bbac48290
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-08 12:21:03 +0530

    dd

--- index.js
+++ index.js
@@ -143,6 +143,7 @@ if( req.body.result.action=== "Incident_Status_Check"){
       IncidentNumber=str.concat(IncidentNumber);
     }
     inc.statusIncident(IncidentNumber,function(err,resu){
+      console.log(resu);
       console.log("Result Check "+resu.hasOwnProperty("result"));
      
       var flag=false;
