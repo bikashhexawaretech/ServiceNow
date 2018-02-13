1df0b2adf03bf62e7c63dd8d17e8242fc75362e0
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-13 12:41:12 +0530

    df

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.post('/',function(req,res){
     var facebookResponse='';
-    console.log("Body: "+req.body);
+    console.log("Body: "+req.body[0]);
  if(req.body.result.action==='IncidentRequestAction'){
   
       facebookResponse={
