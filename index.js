e9a9ba11d023edd73e0a2791d5931a01248aa67c
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-13 11:43:11 +0530

    nd

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.post('/',function(req,res){
     console.log('Received the request & it is:::'+JSON.stringify(req.body));
     var facebookResponse='';
+    console.log(req.body.result.fulfillment.messages[0].platform);
  if(req.body.result.action==='IncidentRequestAction'){
 
       facebookResponse={
