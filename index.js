e089bee68df41e7e47ddf6371194bc71795aad4d
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-14 10:54:19 +0530

    jdjd

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.post('/',function(req,res){
       
         
         inc.logIncident(req.body.result.parameters.desc,req.body.result.parameters.severity,req.body.result.parameters.entityCategory,function(err,resu){
-          console.log("Category: "+req.body.result.parameters.category);
+          console.log("Category: "+req.body.result.parameters.entityCategory);
             var resagent=resu["result"].number+" logged Successfully.";
             
             return res.json({
