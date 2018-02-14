85c666efb700279120790596970979b8789d637b
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-14 21:05:55 +0530

    sdd

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ var inc = require('./app.js');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+'use strict';
+var request = require('http');
+var express=require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var portC = process.env.PORT || 3000;
+var inc = require('./app.js');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 
 
 
@@ -28,39 +38,6 @@ function buildRichResponse(req, res) {
   const NUMBER_INTENT = 'input.number';  
   const NUMBER_ARGUMENT = 'input.mynum'; 
 
-  /*function welcomeIntent (assistant) {
-    assistant.ask(googleAssistant);
-  }
-
-  function numberIntent (assistant) {
-    let number = assistant.getArgument(NUMBER_ARGUMENT);
-    assistant.tell('You said ' + number);
-  }*/
-
- /* let actionMap = new Map();
-  actionMap.set(WELCOME_INTENT, welcomeIntent);
-  actionMap.set(NUMBER_INTENT, numberIntent);
-  assistant.handleRequest(actionMap);*/
-
-  /*
-  function responseHandler (assistant) {
-    console.log("okok")
-    // intent contains the name of the intent you defined in the Actions area of API.AI
-    let intent = assistant.getIntent();
-    switch (intent) {
-      case WELCOME_INTENT:
-        assistant.ask('Welcome! Say a number.');
-        break;
-
-      case NUMBER_INTENT:
-        let number = assistant.getArgument(NUMBER_ARGUMENT);
-        assistant.tell('You said ' + number);
-        break;
-    }
-  }
-  */
-  // you can add the function name instead of an action map
- // assistant.handleRequest(responseHandler);
 }
 
 
@@ -69,13 +46,9 @@ app.post('/', function (req, res) {
   buildRichResponse(req, res);
 })
 
-/*
-app.get('/', function (req, res) {
-  res.send("Server is up and running.")
-})
-*/
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+
+app.listen(portC, function () {
+  console.log('Example app listening on port !'+portC)
 })
