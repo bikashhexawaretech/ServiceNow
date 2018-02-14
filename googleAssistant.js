f4fa6b72c8ca16b3f0a984b3effd942c5b0a5421
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-14 21:48:06 +0530

    kjj

--- googleAssistant.js
+++ googleAssistant.js
@@ -4,7 +4,7 @@ var funResponse= function buildRichResponse(req, res) {
   const assistant = new DialogflowApp({request: req, response: res});
 
   var googleAssistant = assistant.buildRichResponse()
-  .addSimpleResponse({speech: 'Please select option from ',
+  .addSimpleResponse({speech: 'My name is khan and I am not a terrorist ',
     displayText: 'Please select option from '});
   //.addSuggestions('Option-1') ;
   
