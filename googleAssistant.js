8bad0dfb8d2c99d54d7ef18caa4b20c889e0c721
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-15 09:59:49 +0530

    dff

--- googleAssistant.js
+++ googleAssistant.js
@@ -1,3 +1,5 @@
+
+/*
 const DialogflowApp = require('actions-on-google').DialogflowApp;
 
 var funResponse= function buildRichResponse(req, res) {
@@ -6,7 +8,7 @@ var funResponse= function buildRichResponse(req, res) {
 
   //Suggestion Chips
 
-  /*
+  
   var googleAssistant = assistant.buildRichResponse()
   .addSimpleResponse({speech: 'Simple Text Speech',
     displayText: 'Simple Text Display'})
@@ -17,11 +19,10 @@ var funResponse= function buildRichResponse(req, res) {
       ['Basic Card', 'List', 'Carousel', 'Suggestions']);
  
   
-*/
+
   //Basic Card
   
-  /*
-
+ 
   var googleAssistant = assistant.buildRichResponse()
   .addSimpleResponse({speech: 'Simple Text Speech',
     displayText: 'Simple Text Display'})
@@ -36,49 +37,9 @@ var funResponse= function buildRichResponse(req, res) {
           .setImage('https://3c2ba678857e073c9506-9b92ffc51ccdc874f7e956dfcfbdbfba.ssl.cf5.rackcdn.com/spark/images/servicenow/servicenow.png', 'Image alternate text')
         );
 
-        */
 
-      //List
-        
-       var googleAssistant = assistant.buildRichResponse()
-    
-         .addSimpleResponse({
-           speech: 'List',
-           displayText: 'List'})
-            
-           .assistant.buildList('List Title')
-           // Add the first item to the list
-           .addItems(assistant.buildOptionItem('title',
-             ['synonym of title 1', 'synonym of title 2', 'synonym of title 3'])
-             .setTitle('Title of First List Item')
-             .setDescription('This is a description of a list item')
-             .setImage('https://www.synonyms.net/root/app_common/img/top_logo_syn.png', 'Image alternate text'))
-           // Add the second item to the list
-           .addItems(assistant.buildOptionItem('googleHome',
-             ['Google Home Assistant', 'Assistant on the Google Home'])
-             .setTitle('Google Home')
-             .setDescription(`Google Home is a voice-activated speaker powered by
-               the Google Assistant.`)
-             .setImage('https://multimedia.bbycastatic.ca/multimedia/products/500x500/107/10721/10721100.jpg', 'Google Home')
-           )
-           // Add third item to the list
-           .addItems(assistant.buildOptionItem('googlePixel',
-             ['Google Pixel XL', 'Pixel', 'Pixel XL'])
-             .setTitle('Google Pixel')
-             .setDescription('Pixel. Phone by Google.')
-             .setImage('https://www.eglobaldigitalcameras.com.au/images/detailed/37/google-pixel-128gb-black.jpg', 'Google Pixel')
-           )
-           // Add last item of the list
-           .addItems(assistant.buildOptionItem('googleAllo', [])
-             .setTitle('Google Allo')
-             .setDescription('Introducing Google Allo, a smart messaging app' +
-               'that helps you say more and do more.')
-             .setImage('https://allo.google.com/images/allo-logo.png', 'Google Allo Logo')
-             .addSynonyms('Allo')
-           )
-       
+  
      
-
   assistant.ask(googleAssistant);
 
   const WELCOME_INTENT = 'input.welcome';  
@@ -86,5 +47,50 @@ var funResponse= function buildRichResponse(req, res) {
   const NUMBER_ARGUMENT = 'input.mynum'; 
 
 }
+        */
+
+let ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+
+var funResponse= function buildRichResponse(req, res) {
+  const assistant = new ActionsSdkApp({request: req, response: res});
+
+var googleAssistant = assistant.buildRichResponse()
+    
+.addSimpleResponse('This is a simple response for a list')
+.addSuggestions(
+  ['Basic Card', 'List', 'Carousel', 'Suggestions'])
+// Build a list
+.assistant.buildList('List Title')
+  // Add the first item to the list
+  .addItems(assistant.buildOptionItem('title',
+    ['synonym of title 1', 'synonym of title 2', 'synonym of title 3'])
+    .setTitle('Title of First List Item')
+    .setDescription('This is a description of a list item')
+    .setImage('https://www.synonyms.net/root/app_common/img/top_logo_syn.png', 'Image alternate text'))
+  // Add the second item to the list
+  .addItems(assistant.buildOptionItem('googleHome',
+    ['Google Home Assistant', 'Assistant on the Google Home'])
+    .setTitle('Google Home')
+    .setDescription(`Google Home is a voice-activated speaker powered by
+      the Google Assistant.`)
+    .setImage('https://multimedia.bbycastatic.ca/multimedia/products/500x500/107/10721/10721100.jpg', 'Google Home')
+  )
+  // Add third item to the list
+  .addItems(assistant.buildOptionItem('googlePixel',
+    ['Google Pixel XL', 'Pixel', 'Pixel XL'])
+    .setTitle('Google Pixel')
+    .setDescription('Pixel. Phone by Google.')
+    .setImage('https://www.eglobaldigitalcameras.com.au/images/detailed/37/google-pixel-128gb-black.jpg', 'Google Pixel')
+  )
+  // Add last item of the list
+  .addItems(assistant.buildOptionItem('googleAllo', [])
+    .setTitle('Google Allo')
+    .setDescription('Introducing Google Allo, a smart messaging app' +
+      'that helps you say more and do more.')
+    .setImage('https://allo.google.com/images/allo-logo.png', 'Google Allo Logo')
+    .addSynonyms('Allo')
+  )
+  assistant.ask(googleAssistant);
+};
 
 module.exports.BuildRichResponse=funResponse;
