b1f51f78f6eb13b3d760efd5f3e54d7a78e3fc3c
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-19 15:59:12 +0530

    dd

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 'use strict';
 var request = require('http');
 var express=require('express');
@@ -10,130 +11,138 @@ app.use(bodyParser.json());
 
 
 app.post('/',function(req,res){
+    console.log('Received the request & it is:::'+JSON.stringify(req.body));
     var facebookResponse='';
-    var googleResponse='';
-    
-    if(req.body.originalRequest.source==='facebook')
-    {
-      if(req.body.result.action==='IncidentRequestAction'){
-  
-        facebookResponse={
-          "speech": "",
-        "messages": [
-          {
-            "type": 1,
-            "platform": "facebook",
-            "title": "Select Category",
-            "subtitle": "",
-            "imageUrl": "http://www.cromacampus.com/wp-content/uploads/2017/05/servicenow-tool-training.png",
-            "buttons": [
-              {
-                "text": "Hardware",
-                "postback": "Hardware"
-              },
-              {
-                "text": "Software",
-                "postback": "Software"
-              },
-              {
-                "text": "Network",
-                "postback": "Network"
-              }
-            ]
-          }
-          
-          
-        ]
-       }
-       return res.json(facebookResponse);
-      }
-      
-      if( req.body.result.action=== "IncidentWebCall"){
-      
-        inc.logIncident(req.body.result.parameters.desc,req.body.result.parameters.severity,req.body.result.parameters.entityCategory,function(err,resu){
-          console.log("Severity :"+req.body.result.parameters.severity);
-            var resagent=resu["result"].number+" logged Successfully.";
-            
-            return res.json({
-              speech:resagent,
-              displayText: resagent,
-              source:'',
-              followupEvent: {
-                "name": "eventSuccessProceed",
-                "data": {
-                    "Incident":resu["result"].number,
-                    "Description":req.body.result.parameters.desc,
-                    "Category":req.body.result.parameters.category,
-                    "Urgency":req.body.result.parameters.urgency
-                }
-             }
-            });
-    })
-     
-  }
-  
-  
-   
-    }
-    else  if(req.body.originalRequest.source=='google'){
-      if(req.body.result.action==='IncidentRequestAction'){
-  
-        googleResponse={
-          "speech": "",
-        "messages": [
-          
-          {
-            "type": "simple_response",
-            "platform": "google",
-            "textToSpeech": "Please select one of the following options"
-          },
-          {
-            "type": "suggestion_chips",
-            "platform": "google",
-            "suggestions": [
-              {
-                "title": "Hardware"
-              },
-              {
-                "title": "Software"
-              },
-              {
-                "title": "Network"
-              }
-            ]
-          },
-        ]
-       }
-       return res.json(googleResponse);
-      }
+ if(req.body.result.action==='IncidentRequestAction'){
 
-      if( req.body.result.action=== "IncidentWebCall"){
-      
-        
-        inc.logIncident(req.body.result.parameters.desc,req.body.result.parameters.severity,req.body.result.parameters.entityCategory,function(err,resu){
-          console.log(req.body.result.parameters.desc);
-            var resagent=resu["result"].number+" logged Successfully.";
-            
-            return res.json({
-              speech:resagent,
-              displayText: resagent,
-              source:'',
-              followupEvent: {
-                "name": "eventSuccessProceed",
-                "data": {
-                    "Incident":resu["result"].number,
-                    "Description":req.body.result.parameters.desc,
-                    "Category":req.body.result.parameters.category,
-                    "Urgency":req.body.result.parameters.urgency
-                }
-             }
-             
-            });
-    })
-     
-  }
+      facebookResponse={
+        "speech": "",
+      "messages": [
+        {
+          "type": 1,
+          "platform": "facebook",
+          "title": "Select Category",
+          "subtitle": "",
+          "imageUrl": "http://www.cromacampus.com/wp-content/uploads/2017/05/servicenow-tool-training.png",
+          "buttons": [
+            {
+              "text": "Hardware",
+              "postback": "Hardware"
+            },
+            {
+              "text": "Software",
+              "postback": "Software"
+            },
+            {
+              "text": "Network",
+              "postback": "Network"
+            }
+          ]
+        },
+        {
+          "type": 0,
+          "speech": ""
+        }
+      ]
+     }
+     return res.json(facebookResponse);
     }
+    
+   /* if( req.body.result.action=== "Incident_Request.Incident_Request-custom" ){
+     
+         facebookResponse={
+           "speech": "",
+           "messages": [
+             {
+               "type": 2,
+               "platform": "facebook",
+               "title": "Select Urgency",
+               "replies": [
+                 "High",
+                 "Low",
+                 "Medium"
+               
+               ]
+             },
+             {
+               "type": 0,
+               "speech": ""
+             }
+           ]
+        }
+        return res.json(facebookResponse);
+}*/
+/*else
+if( req.body.result.action=== "Incident_Request.Incident_Request-custom" && req.body.result.parameters.entityCategory==='Software'){
+     facebookResponse={
+       "speech": "",
+       "messages": [
+         {
+           "type": 2,
+           "platform": "facebook",
+           "title": "Select Sub Categories",
+           "replies": [
+             "Email",
+             "OS" 
+           ]
+         },
+         {
+           "type": 0,
+           "speech": ""
+         }
+       ]
+    }
+    return res.json(facebookResponse);
+}
+ else
+ if( req.body.result.action=== "Incident_Request.Incident_Request-custom" && req.body.result.parameters.entityCategory==='Network'){
+     facebookResponse={
+       "speech": "",
+       "messages": [
+         {
+           "type": 2,
+           "platform": "facebook",
+           "title": "Select Sub Categories",
+           "replies": [
+             "DHCP",
+             "DNS",
+             "VPN" 
+           ]
+         },
+         {
+           "type": 0,
+           "speech": ""
+         }
+       ]
+    }
+    return res.json(facebookResponse);
+}*/
+
+
+  if( req.body.result.action=== "IncidentWebCall"){
+    
+ 
+      inc.logIncident(req.body.result.parameters.desc,req.body.result.parameters.severity,req.body.result.parameters.entityCategory,function(err,resu){
+        console.log(req.body.result.parameters.desc);
+          var resagent=resu["result"].number+" logged Successfully.";
+          
+          return res.json({
+            speech:resagent,
+            displayText: resagent,
+            source:'',
+            followupEvent: {
+              "name": "eventSuccessProceed",
+              "data": {
+                  "Incident":resu["result"].number,
+                  "Description":req.body.result.parameters.desc,
+                  "Category":req.body.result.parameters.category,
+                  "Urgency":req.body.result.parameters.urgency
+              }
+           }
+          });
+  })
    
+}
 
 // Simple text sent to FB
 
@@ -217,6 +226,146 @@ if( req.body.result.action=== "Incident_Status_Check"){
 
 }
 });
+
+
+/*
+
+if( req.body.result.action=== "Incident_Status_Check"){
+  var IncidentNumber=req.body.result.parameters.IncidentNumber;
+  if(IncidentNumber.indexOf('INC') == -1)
+  {
+    var str='INC';
+    IncidentNumber=str.concat(IncidentNumber);
+  }
+  inc.statusIncident(IncidentNumber,function(err,resu){
+    var JSONOBJ=JSON.parse(resu);
+   
+    console.log("Result Check "+JSONOBJ.hasOwnProperty("result"));
+   
+    facebookResponse={
+      "speech": "",
+      "messages": [
+        {
+          "type": 4,
+          "platform": "facebook",
+          "payload": {
+            "facebook": {
+              "attachment": {
+                "type": "template",
+                "payload": {
+                  "template_type": "receipt",
+                  "recipient_name": "Stephane Crozatier",
+                  "order_number": "12345678902",
+                  "currency": "USD",
+                  "payment_method": "Visa 2345",
+                  "order_url": "http://petersapparel.parseapp.com/order?order_id=123456",
+                  "timestamp": "1428444852",
+                  "address": {
+                    "street_1": "1 Hacker Way",
+                    "street_2": "",
+                    "city": "Menlo Park",
+                    "postal_code": "94025",
+                    "state": "CA",
+                    "country": "US"
+                  },
+                  "summary": {
+                    "subtotal": 75,
+                    "shipping_cost": 4.95,
+                    "total_tax": 6.19,
+                    "total_cost": 56.14
+                  },
+                  "adjustments": [
+                    {
+                      "name": "New Customer Discount",
+                      "amount": 20
+                    },
+                    {
+                      "name": "$10 Off Coupon",
+                      "amount": 10
+                    }
+                  ],
+                  "elements": [
+                    {
+                      "title": IncidentNumber,
+                      "subtitle": JSONOBJ.result[0].short_description,
+                      "quantity": 2,
+                      "price": 50,
+                      "currency": "USD",
+                      "image_url": "http://petersapparel.parseapp.com/img/whiteshirt.png"
+                    } 
+                  ]
+                }
+              }
+            }
+          }
+        },
+        {
+          "type": 0,
+          "speech": "Hi!"
+        }
+      ]
+    
+   }
+   return res.json(facebookResponse);
+
+  
+  })
+   
+    
+ 
+
+}
+});
+
+*/
+/*
+app.post('/',function(req,res){
+    console.log('Received the request & it is:::'+JSON.stringify(req.body));
+
+ if(req.body.result.action==='Incident_Request.Incident_Request-custom'){
+    var facebookResponse={
+        "speech": "",
+        "displayText": " Your Incident has been raised",
+        "data": {
+          "facebook": {
+
+            "attachment": {
+                  "type": "template",
+                  "payload": {
+                  "template_type": "generic",
+                  "elements": [
+                    {
+                      "title": "Select SubCategory",
+                      "subtitle": '',
+                      "buttons": [
+                        {
+                          "type": "postback",
+                          "title": "CPU",
+                          "payload": "CPU"
+                        },
+                        {
+                          "type": "postback",
+                          "title": "Keyboard",
+                          "payload": "Keyboard"
+                        }
+                      ]
+                    }
+                  ]
+                }
+              }
+            }
+          },
+      
+        };
+
+return res.json(facebookResponse);
+ 
+    } 
+});*/
+
+
+
+  
 
 
 
