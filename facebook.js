97ea39ec36d4c78bee774a57108fa12659e9ff5b
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-21 14:18:12 +0530

    jdj

--- facebook.js
+++ facebook.js
@@ -66,18 +66,43 @@ var genTemplate=function genTemplate(){
 
     var facebookResponse={
         "speech": "",
-      "messages": [
-        {
-            
-            "attachment": {
-                "type": "audio",
-                "payload": {
-                  "url": "https://rockets.chatfuel.com/"
-                }
-              }
-              
-        }
-    ]
+            "messages": [
+               {
+                 "attachment":{
+                   "type":"template",
+                   "payload":{
+                     "template_type":"list",
+                     "top_element_style":"large",
+                     "elements":[
+                       {
+                         "title":"Chatfuel Rockets T-Shirt",
+                         "image_url":"http://rockets.chatfuel.com/img/shirt.png",
+                         "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
+                         "buttons":[
+                           {
+                             "type":"web_url",
+                             "url":"https://rockets.chatfuel.com/store/shirt",
+                             "title":"View Item"
+                           }
+                         ]
+                       },
+                       {
+                         "title":"Chatfuel Rockets Hoodie",
+                         "image_url":"http://rockets.chatfuel.com/img/hoodie.png",
+                         "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
+                         "buttons":[
+                           {
+                             "type":"web_url",
+                             "url":"https://rockets.chatfuel.com/store/hoodie",
+                             "title":"View Item"
+                           }
+                         ]
+                       }
+                     ]
+                   }
+                 }
+               }
+             ]
 }
      return facebookResponse;
   }
