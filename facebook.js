b6c1bd507d4f178e8fc259ea1c4193257885e537
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-21 15:17:52 +0530

    FB Custom payload

--- facebook.js
+++ facebook.js
@@ -62,55 +62,212 @@
   }
 
 
-var genTemplate=function genTemplate(){
-
-    var facebookResponse={
-        "speech": "",
-            "messages": [
-               {
-                 "attachment":{
-                   "type":"template",
-                   "payload":{
-                     "template_type":"list",
-                     "top_element_style":"large",
-                     "elements":[
-                       {
-                         "title":"Chatfuel Rockets T-Shirt",
-                         "image_url":"http://rockets.chatfuel.com/img/shirt.png",
-                         "subtitle":"Soft white cotton t-shirt with CF Rockets logo",
-                         "buttons":[
-                           {
-                             "type":"web_url",
-                             "url":"https://rockets.chatfuel.com/store/shirt",
-                             "title":"View Item"
-                           }
-                         ]
-                       },
-                       {
-                         "title":"Chatfuel Rockets Hoodie",
-                         "image_url":"http://rockets.chatfuel.com/img/hoodie.png",
-                         "subtitle":"Soft gray cotton t-shirt with CF Rockets logo",
-                         "buttons":[
-                           {
-                             "type":"web_url",
-                             "url":"https://rockets.chatfuel.com/store/hoodie",
-                             "title":"View Item"
-                           }
-                         ]
-                       }
-                     ]
-                   }
-                 }
-               }
-             ]
-}
-     return facebookResponse;
-  }
 
   module.exports.fbQuickReply=fbQuickReply;
   module.exports.fbCard=fbCard;
-  module.exports.genTemplate=genTemplate;
+ 
+
+  // Custom payload
+
+  //List
+
+  /*
+
+  {
+    "facebook": {
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"receipt",
+          "recipient_name":"Stephane Crozatier",
+          "order_number":"12345678902",
+          "currency":"USD",
+          "payment_method":"Visa 2345",        
+          "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+          "timestamp":"1428444852",         
+          "address":{
+            "street_1":"1 Hacker Way",
+            "street_2":"",
+            "city":"Menlo Park",
+            "postal_code":"94025",
+            "state":"CA",
+            "country":"US"
+          },
+          "summary":{
+            "subtotal":75.00,
+            "shipping_cost":4.95,
+            "total_tax":6.19,
+            "total_cost":56.14
+          },
+          "adjustments":[
+            {
+              "name":"New Customer Discount",
+              "amount":20
+            },
+            {
+              "name":"$10 Off Coupon",
+              "amount":10
+            }
+          ],
+          "elements":[
+            {
+              "title":"Classic White T-Shirt",
+              "subtitle":"100% Soft and Luxurious Cotton",
+              "quantity":2,
+              "price":50,
+              "currency":"USD",
+              "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
+            },
+            {
+              "title":"Classic Gray T-Shirt",
+              "subtitle":"100% Soft and Luxurious Cotton",
+              "quantity":1,
+              "price":25,
+              "currency":"USD",
+              "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
+            }
+          ]
+        }
+      }
+    
+    }
+  }
 
 
 
   
+  */
+
+
+  //button template-custome payload
+
+  /*
+  {
+    "facebook": {
+      
+        "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"button",
+          "text":"What do you want to do next?",
+          "buttons":[
+            {
+              "type":"web_url",
+              "url":"https://www.messenger.com",
+              "title":"Visit Messenger"
+            }
+             
+          ]
+        }
+      }
+    
+    
+    
+    }
+  }
+  */
+
+
+  //open graph
+
+  /*
+
+
+  "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"open_graph",
+        "elements":[
+           {
+            "url":"https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://en.wikipedia.org/wiki/Rickrolling",
+                "title":"View More"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
+  }
+
+  */
+
+
+  //airline pass
+
+  /*
+
+  {
+  "facebook": {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "airline_boardingpass",
+        "intro_message": "You are checked in.",
+        "locale": "en_US",
+        "boarding_pass": [
+          {
+            "passenger_name": "SMITH\/NICOLAS",
+            "pnr_number": "CG4X7U",
+            "seat": "74J",            
+            "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
+            "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
+            "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
+            "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
+            "auxiliary_fields": [
+              {
+                "label": "Terminal",
+                "value": "T1"
+              },
+              {
+                "label": "Departure",
+                "value": "30OCT 19:05"
+              }
+            ],
+            "secondary_fields": [
+              {
+                "label": "Boarding",
+                "value": "18:30"
+              },
+              {
+                "label": "Gate",
+                "value": "D57"
+              },
+              {
+                "label": "Seat",
+                "value": "74J"
+              },
+              {
+                "label": "Sec.Nr.",
+                "value": "003"
+              }
+            ],
+            "flight_info": {
+              "flight_number": "KL0642",
+              "departure_airport": {
+                "airport_code": "JFK",
+                "city": "New York",
+                "terminal": "T1",
+                "gate": "D57"
+              },
+              "arrival_airport": {
+                "airport_code": "AMS",
+                "city": "Amsterdam"
+              },
+              "flight_schedule": {
+                "departure_time": "2016-01-02T19:05",
+                "arrival_time": "2016-01-05T17:30"
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
+  }
+
+
+  */
