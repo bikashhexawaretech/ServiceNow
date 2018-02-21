3c752b026720a665268b9f5df0e8241ddeeebba8
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-21 14:14:58 +0530

    jsh

--- facebook.js
+++ facebook.js
@@ -68,49 +68,17 @@ var genTemplate=function genTemplate(){
         "speech": "",
       "messages": [
         {
-            "type": 4,
-          "platform": "facebook",
-          "payload": {
-            "facebook": {
-              "attachment": {
-                "type": "",
+            
+            "attachment": {
+                "type": "audio",
                 "payload": {
-                  "template_type": "generic",
-                  "elements": [
-                    {
-                      "title": "Welcome!",
-                      "image_url": "https://petersfancybrownhats.com/company_image.png",
-                      "subtitle": "We have the right hat for everyone.",
-                      "default_action": {
-                        "type": "web_url",
-                        "url": "https://petersfancybrownhats.com/view?item=103",
-                        "messenger_extensions": false,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://petersfancybrownhats.com/"
-                      },
-                      "buttons": [
-                        {
-                          "type": "web_url",
-                          "url": "https://petersfancybrownhats.com",
-                          "title": "View Website"
-                        },
-                        {
-                          "type": "postback",
-                          "title": "Start Chatting",
-                          "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                        }
-                      ]
-                    }
-                  ]
+                  "url": "https://rockets.chatfuel.com/"
                 }
               }
-            }
-          }
+              
         }
-        
-        
-      ]
-     }
+    ]
+}
      return facebookResponse;
   }
 
