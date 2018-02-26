cf159c547ea0994aed797ec4cf72783bd870a13b
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-26 16:02:27 +0530

    ss

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var request = require("request");
 function getProfile (cb) {
     request({
      method: 'GET',
-     uri: 'https://graph.facebook.com/me',
+     uri: 'https://graph.facebook.com/me ',
      qs: {
       
    access_token: 'EAACEdEose0cBADjrLL4irKQ5jJJAMtqAZBpWilFTRRZCiorZC5H37wvNypUZAF5ohBCvJOaDzvCFGWPR83dhIa1aQtkHiq9yx56bLT0qfK3soRTYHqn8s99uAMWaAbgIJZCijcWpvRZBcWitadTdpBuq5GAzeKgRnKRhOZBhLuXKjrlnVjkN20GWQ97gtHXjYQZD'
