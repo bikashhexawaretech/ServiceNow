3428f98f22edf4bb0b7eb876fa630b57d3a38f69
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-27 22:24:47 +0530

    sss

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 var facebook=require('./facebook.js');
 app.use(express.static('public'));
 
-app.get('/servicenow', function (req, res) {
+app.get('/', function (req, res) {
 console.log('GET CALL');
   return res.json({
     speech:'hi',
