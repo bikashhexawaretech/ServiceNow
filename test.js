7196620268bfafee4d6f5f05dcbf720de540a518
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-28 11:17:00 +0530

    sd

--- test.js
+++ test.js
@@ -1,18 +1,6 @@
-var facebookAuthEndpoint = "https://www.facebook.com/v2.10/dialog/oauth";
-var responseType = "token";
-var clientId = "173489109936826";  
-var redirectUrl = "http://travelbuddyapp.com:8080/callback.html";
-var scope = "public_profile user_friends";
+var facebookAuthEndpoint = "https://www.facebook.com/v2.10/dialog/oauth?";
+var clientId = "173489109936826";  //service now
+var redirectUrl = "https://servicenowhex.herokuapp.com/";
+var url=facebookAuthEndpoint+'app_id='+clientId+'&redirect_uri='+redirectUrl;
 
-
-var userEndpoint = "https://graph.facebook.com/v2.10/me?fields=name&access_token="+ accessToken;
-  var userId = "";
-  //ajax GET call to get user ID
-  $.get(userEndpoint, function(data, status){
-    userId = data.id;
-      var friendListEndpoint = "https://graph.facebook.com/v2.10/" + userId + 
-"/friends?access_token=" + accessToken;
-      $.get(friendListEndpoint, function(data, status){
-            $("#response").html(JSON.stringify(data));
-      });
-  });
+console.log(url);
