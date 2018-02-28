4828a079821bfea1fd92ce7cb50b5e10411841ae
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-28 18:40:14 +0530

    jhdg

--- facebook.js
+++ facebook.js
@@ -177,19 +177,19 @@ var fbGeneric=function Generic(){
 }
   return facebookResponse;
 }
- /*
+ 
 var facebookAuthEndpoint = "https://www.facebook.com/v2.10/dialog/oauth?";
 var clientId = "173489109936826";  //service now
 var redirectUrl = "https://servicenowhex.herokuapp.com/";
 var url=facebookAuthEndpoint+'app_id='+clientId+'&redirect_uri='+redirectUrl;
-*/
 
+/*
 
 var facebookAuthEndpoint="https://accounts.google.com/o/oauth2/v2/auth?";
 var clientId = "657702593596-95ainiutsgmm95eipvf5f6eqe7t45rhu.apps.googleusercontent.com";  //service now
 var redirectUrl = "https://servicenowhex.herokuapp.com";
 var url=facebookAuthEndpoint+'client_id='+clientId+'&redirect_uri='+redirectUrl+'&approval_prompt=force&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline';
-
+*/
 var fbWebView=function webView(){
   var facebookResponse={
     "speech":"",
