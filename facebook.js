b71888e41a6a28a8d867ec82ba8fc4fbb6ae4032
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-28 18:00:12 +0530

    gdf

--- facebook.js
+++ facebook.js
@@ -204,12 +204,14 @@ var fbWebView=function webView(){
                 "text": "Try the log in button!",
                 "buttons": [
                   {
-                    "type":"web_url",
-                    "url":url,
+                    "type":"account_link",
+                    "url":url
+                    /*
                     "title":"LogIn",
                     "webview_height_ratio": "full",
                     "messenger_extensions": "true",  
                     "fallback_url": "https://www.facebook.com/"
+                    */
                    
                   //"url": "https://jaspers-market.herokuapp.com/users/login?account_linking_token=ARQoGXK8uIhpN8Y860wJIx7GLdLMhpFvUhCfbQRygxURkJ8BqrD78aK1EcMbpfZt5urB16gw0ZUB0ZHxjtfrTZ7tIMnqOtnxnOifZplmCIqYgw&redirect_uri=https%3A%2F%2Ffacebook.com%2Fmessenger_platform%2Faccount_linking%2F%3Faccount_linking_token%3DARQoGXK8uIhpN8Y860wJIx7GLdLMhpFvUhCfbQRygxURkJ8BqrD78aK1EcMbpfZt5urB16gw0ZUB0ZHxjtfrTZ7tIMnqOtnxnOifZplmCIqYgw"
                   }
