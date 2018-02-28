07ec44992264980b6a6b77cd63f3ca8030c1e8f6
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-02-28 14:08:36 +0530

    zz

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(bodyParser.json());
 var facebook=require('./facebook.js');
 app.use(express.static('public'));
 
- app.get('/?code',function(req,res){
-
+ app.get('/',function(req,res){
+   console.log(req);
+console.log(req.query, req.params);
   console.log('code '+req.query.code);
  })
 
