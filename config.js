2c477d541452a9f02066d9b6773e4fa313ff0989
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-01 15:18:18 +0530

    sdd

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ var config = {
     "serviceNowUserName": "33238",
     "serviceNowPassword": "abc123",
     "serviceNowVersion": "v1",
-    "serviceNowTableName": "incident",
+    
     "clientAccessToken": "1c5c2bd1f8b548b18f3782ca17420f2c",
     "getIncidentUrl": "https://dev18442.service-now.com/api/now/v1/table/incident?number=",
     "authOClientId": "xULQxRpZ5JfoRSmrh8TMmzNEDPgK03qs",
     "authOClientSecretKey": "OxAMgfawOjizGe63ezqOlKQ9U7OLZna2OT0s4R2iWPEEK0Ua_Magy8PWrGZI4Y4O",
     "authODomain": "bikashpanigrahi.auth0.com",
-    "authOCallbackUrl": "https://https://servicenowhex.herokuapp.com/callback",
+    "authOCallbackUrl": "https://servicenowhex.herokuapp.com/callback",
     "facebookPageAccessToken": "EAAFwXfBX3n4BAPyrwV5cq8pOHaYPu8KKOrAiyz14lDtTlBCgu3cbs5tqsFNd5HItSyng3qUZCecWMANWDorPDQvFkhsH0KZCqMiFLJEpf6l86PpKVFW0EiS40iHqi4T7F7pSVUgOSlDzonItWpSogOW7fwgzw0884PTeZBYUQZDZD",
     "facebookMessageUri":"https://graph.facebook.com/v2.6/me/messages"
 };
