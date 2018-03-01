f8bdd183b4969027008f04d03a2cae4e53b416c0
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-01 12:28:02 +0530

    ddd

--- facebook.js
+++ facebook.js
@@ -181,7 +181,7 @@ var fbGeneric=function Generic(){
 
 var facebookAuthEndpoint = "https://www.facebook.com/v2.10/dialog/oauth?";
 var clientId = "173489109936826";  //service now
-var redirectUrl = "/auth/facebook/callback/";
+var redirectUrl = "https://servicenowhex.herokuapp.com/";
 var url=facebookAuthEndpoint+'app_id='+clientId+'&redirect_uri='+redirectUrl;
 
 
