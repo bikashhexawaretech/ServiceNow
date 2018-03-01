cd03263cff82432d297f0e3b01207fb40864a21e
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-01 11:14:39 +0530

    dd

--- facebook.js
+++ facebook.js
@@ -199,8 +199,8 @@ var clientSecret="qo9_ipjpykVrpjsjiozy6a6y";
 //var url=facebookAuthEndpoint+'client_id='+clientId+'&access_token_url='+Accesstokenurl+'&auth_url='+AuthUrl+'&scope='+scope+'&redirect_uri='+redirectUrl+'&approval_prompt=force&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline';
 //var url=facebookAuthEndpoint+'client_id='+clientId+'&redirect_uri='+redirectUrl+'&response_type=code'+'&scope='+scope+'&login_hint=jsmith@example.com&openid.realm=example.com&nonce=0394852-3190485-2490358&hd=example.com';
 //var url=facebookAuthEndpoint+'client_secret='+clientSecret+'&client_id='+clientId+'&response_type=code'+'&scope='+scope+'&grant_type=authorization_code';
-var url='https://accounts.google.com/o/oauth2/auth?client_id=1096250870035-f72i9rckit64qpejr89ronu7s4m3qd9j.apps.googleusercontent.com&response_type=code&redirect_uri=https://servicenowhex.herokuapp.com&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline';
-
+//var url='https://accounts.google.com/o/oauth2/auth?client_id=1096250870035-f72i9rckit64qpejr89ronu7s4m3qd9j.apps.googleusercontent.com&response_type=code&redirect_uri=https://servicenowhex.herokuapp.com&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline';
+var url='https://accounts.google.com/o/oauth2/auth?client_id=370289834912-8ar3utqcn3oj78dsbfo1ms1sj4f51oej.apps.googleusercontent.com&response_type=code&redirect_uri=https://servicenowtraining.herokuapp.com/&scope=https://www.googleapis.com/auth/drive.metadata&access_type=offline';
 var fbWebView=function webView(){
   var facebookResponse={
     "speech":"",
