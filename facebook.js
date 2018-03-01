1048d9bfeb717e601d0555f1651a0e06c33ebea9
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-01 11:55:51 +0530

    sss

--- facebook.js
+++ facebook.js
@@ -187,7 +187,7 @@ var url=facebookAuthEndpoint+'app_id='+clientId+'&redirect_uri='+redirectUrl;
 */
 
 
-
+/*
 var facebookAuthEndpoint="https://accounts.google.com/o/oauth2/token?";
 var clientId = "1096250870035-7bdfg2fjgi3dma1p6b0ptqg39h7cq4gs.apps.googleusercontent.com";  //service now
 //var redirectUrl = "https://servicenowhex.herokuapp.com";
@@ -200,6 +200,8 @@ var clientSecret="qo9_ipjpykVrpjsjiozy6a6y";
 //var url=facebookAuthEndpoint+'client_id='+clientId+'&redirect_uri='+redirectUrl+'&response_type=code'+'&scope='+scope+'&login_hint=jsmith@example.com&openid.realm=example.com&nonce=0394852-3190485-2490358&hd=example.com';
 //var url=facebookAuthEndpoint+'client_secret='+clientSecret+'&client_id='+clientId+'&response_type=code'+'&scope='+scope+'&grant_type=authorization_code';
 var url='https://accounts.google.com/o/oauth2/auth?client_id=1096250870035-f72i9rckit64qpejr89ronu7s4m3qd9j.apps.googleusercontent.com&response_type=code&redirect_uri=https://servicenowhex.herokuapp.com&scope=https://www.googleapis.com/auth/userinfo.email&access_type=offline';
+
+*/
 
 var fbWebView=function webView(){
   var facebookResponse={
