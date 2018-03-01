86d9045e17372c08c846711ec9ce4ce612dfc9d3
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-01 15:17:56 +0530

    Check

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ var config = {
     "serviceNowTableName": "incident",
     "clientAccessToken": "1c5c2bd1f8b548b18f3782ca17420f2c",
     "getIncidentUrl": "https://dev18442.service-now.com/api/now/v1/table/incident?number=",
-    "authOClientId": "4hm1dkhFoPFwBkCHYdvHqD2uAMtWdsKK",
-    "authOClientSecretKey": "ZCbJz00XnSuUBCU2E92U3t9OTKnbmXxXUyp7J_MIgHFyFQjzQA52cunV2cuAoxJ4",
-    "authODomain": "report-it.auth0.com",
-    "authOCallbackUrl": "https://report-incident-bot.herokuapp.com/callback",
+    "authOClientId": "xULQxRpZ5JfoRSmrh8TMmzNEDPgK03qs",
+    "authOClientSecretKey": "OxAMgfawOjizGe63ezqOlKQ9U7OLZna2OT0s4R2iWPEEK0Ua_Magy8PWrGZI4Y4O",
+    "authODomain": "bikashpanigrahi.auth0.com",
+    "authOCallbackUrl": "https://https://servicenowhex.herokuapp.com/callback",
     "facebookPageAccessToken": "EAAFwXfBX3n4BAPyrwV5cq8pOHaYPu8KKOrAiyz14lDtTlBCgu3cbs5tqsFNd5HItSyng3qUZCecWMANWDorPDQvFkhsH0KZCqMiFLJEpf6l86PpKVFW0EiS40iHqi4T7F7pSVUgOSlDzonItWpSogOW7fwgzw0884PTeZBYUQZDZD",
     "facebookMessageUri":"https://graph.facebook.com/v2.6/me/messages"
 };
