10bd44b05bc3d37fa3d9e49d576f15ecb5efd0b9
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-05 10:34:45 +0530

    FB Token

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 var config = {
-    "serviceNowInstance": "dev18442",
-    "serviceNowUserName": "33238",
-    "serviceNowPassword": "abc123",
-    "serviceNowVersion": "v1",
-    
+         
     "clientAccessToken": "1c5c2bd1f8b548b18f3782ca17420f2c",
     "getIncidentUrl": "https://dev18442.service-now.com/api/now/v1/table/incident?number=",
     "authOClientId": "xULQxRpZ5JfoRSmrh8TMmzNEDPgK03qs",
     "authOClientSecretKey": "OxAMgfawOjizGe63ezqOlKQ9U7OLZna2OT0s4R2iWPEEK0Ua_Magy8PWrGZI4Y4O",
     "authODomain": "bikashpanigrahi.auth0.com",
     "authOCallbackUrl": "https://servicenowhex.herokuapp.com/callback",
-    "facebookPageAccessToken": "EAACdyZATFKroBAFmXBlN6aa5GzXJ0wh6sPR8t5qNJX6LGFnbauQYa30Qd1Cb8Q19AbZAzxYyZAP16Ur1UAvQvQ4lJFz0xEnCUmrPsiZCOyOV1L46ZCMy7xTUvb9yQFVCLrsTSMVLULGMas7SQFBa210h5FEbLACBX4dkIM0LbFgZDZD",
+    "facebookPageAccessToken": "EAACdyZATFKroBALFJ119ZBi8Sx6IkmdKpwhZCSZBFCxn3BkxXPicxZCbTuBPv8RbfQUF1EkgOcsGUWZCRw6MZA06X1QZBvSfWlrJSQna5KhKh6V2Fn5nkyPN3uLEgghlBDvmadQKQiWvtZB0gRHWhm3EWmZBmhDCApXk9FiUCgVQnAaQZDZD",
     "facebookMessageUri":"https://graph.facebook.com/v2.6/me/messages"
 };
 
