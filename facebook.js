4f6239075b9b1ff8cf5648ef4737f7c004e34325
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-05 12:27:30 +0530

    sdd

--- facebook.js
+++ facebook.js
@@ -220,7 +220,7 @@ var fbWebView=function webView(){
                 "buttons": [
                   {
                     "type":"account_link",
-                    "url":"https://servicenowhex.herokuapp.com/"
+                    "url":url
                     /*
                     "title":"LogIn",
                     "webview_height_ratio": "full",
