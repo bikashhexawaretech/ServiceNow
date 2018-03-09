3fb96c1487843d3c86268b4b7131193c318fb60a
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-09 10:48:33 +0530

    WriteStream

--- app.js
+++ app.js
@@ -54,11 +54,16 @@ function logChatHistory(user, message,botResponse) {
   
   var log = "User "+user + ": " + message + "\n";
   log += "Bot: " + botResponse + "\n\n";
-
+/*
   fs.appendFile("logs.txt", log, function(err) {
 
     console.log(err);
   });
+  */
+ var stream = fs.createWriteStream('logs.txt', {flags: 'a'});
+stream.write(log, function() {
+  // Now the data has been written.
+});
 
   
 } 
