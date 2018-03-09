39bd3c81e786ad6a0a393843d0bc9434e4eb6e05
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-09 14:48:04 +0530

    commit

--- test.js
+++ test.js
@@ -1,24 +1,20 @@
-var http = require("http");
-var options = {
-  hostname: 'www.postcatcher.in',
-  port: 80,
-  path: '/catchers/544b09b4599c1d0200000289',
-  method: 'POST',
-  headers: {
-      'Content-Type': 'application/json',
-  }
-};
-var req = http.request(options, function(res) {
-  console.log('Status: ' + res.statusCode);
-  console.log('Headers: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (body) {
-    console.log('Body: ' + body);
+var fs = require("fs");
+function logChatHistory(user, message,botResponse) {
+  
+  var log = "User "+user + ": " + message + "\n";
+  log += "Bot: " + botResponse + "\n\n";
+/*
+  fs.appendFile("logs.txt", log, function(err) {
+
+    console.log(err);
   });
+  */
+ var stream = fs.createWriteStream('logs.txt', {flags: 'a'});
+stream.write(log, function() {
+  // Now the data has been written.
 });
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
-// write data to request body
-req.write('{"string": "Hello, World"}');
-req.end();
+
+  
+} 
+
+logChatHistory('user','hi','hey');
