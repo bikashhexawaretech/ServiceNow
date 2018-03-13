ea9df34688840d593d46909ef98c9c7eacbd6422
Bikash Panigrahi <m17517hexaware@gmail.com>
2018-03-13 12:30:03 +0530

    dd

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
-var config = {
-         
-    "clientAccessToken": "1c5c2bd1f8b548b18f3782ca17420f2c",
-    "getIncidentUrl": "https://dev18442.service-now.com/api/now/v1/table/incident?number=",
-    "authOClientId": "xULQxRpZ5JfoRSmrh8TMmzNEDPgK03qs",
-    "authOClientSecretKey": "OxAMgfawOjizGe63ezqOlKQ9U7OLZna2OT0s4R2iWPEEK0Ua_Magy8PWrGZI4Y4O",
-    "authODomain": "bikashpanigrahi.auth0.com",
-    "authOCallbackUrl": "https://servicenowhex.herokuapp.com/callback",
-    "facebookPageAccessToken": "EAACdyZATFKroBALFJ119ZBi8Sx6IkmdKpwhZCSZBFCxn3BkxXPicxZCbTuBPv8RbfQUF1EkgOcsGUWZCRw6MZA06X1QZBvSfWlrJSQna5KhKh6V2Fn5nkyPN3uLEgghlBDvmadQKQiWvtZB0gRHWhm3EWmZBmhDCApXk9FiUCgVQnAaQZDZD",
-    "facebookMessageUri":"https://graph.facebook.com/v2.6/me/messages"
-    
+let config = {
+    user: 'sa',
+    password: '12345',
+    server: '127.0.0.1', 
+    database: 'ServiceNow' 
 };
+   
 
-module.exports = config;
+  module.exports = config;
